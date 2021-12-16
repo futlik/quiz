61cc9e9e95185eea8526251a323adc4de05d0a23
futlik <futlik@gmail.com>
2021-12-16 22:44:09 +0300

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -6,41 +6,53 @@ import java.io.IOException;
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
+  private File file; // final
   public synchronized void setFile(File f) {
     file = f;
   }
   public synchronized File getFile() {
     return file;
   }
+  
+  // File as args
+  // static
   public String getContent() throws IOException {
+    // use library
+    // use bufferedStream
+    // close - try with resource
     FileInputStream i = new FileInputStream(file);
-    String output = "";
+    String output = ""; // use StringBuilder 
     int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+    while ((data = i.read()) > 0) { // read - size ? byte
+      output += (char) data; // encoding ?
     }
     return output;
   }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
+  public String getContentWithoutUnicode() throws IOException { // remove throws?
+    FileInputStream i = new FileInputStream(file); 
     String output = "";
     int data;
     while ((data = i.read()) > 0) {
-      if (data < 0x80) {
+      if (data < 0x80) { // magic
         output += (char) data;
       }
     }
     return output;
   }
-  public void saveContent(String content) {
-    FileOutputStream o = new FileOutputStream(file);
-    try {
-      for (int i = 0; i < content.length(); i += 1) {
+  
+  // to static
+  // to another class
+  // throws ?
+  public void saveContent(String content) { // use library
+   
+    FileOutputStream o = new FileOutputStream(file); // use buffered
+    try { // with resource
+      for (int i = 0; i < content.length(); i += 1) { // encoding to byte
         o.write(content.charAt(i));
       }
     } catch (IOException e) {
       e.printStackTrace();
     }
+    // close
   }
 }
